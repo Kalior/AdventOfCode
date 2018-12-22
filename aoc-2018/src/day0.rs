12085a109057a92012c81c95ec60041a9d684467
Joel Gustafsson <me@joelgustafsson.com>
2018-12-22 19:16:08 +0100

    Add parse to day 0

--- aoc-2018/src/day0.rs
+++ aoc-2018/src/day0.rs
@@ -1,8 +1,21 @@
 pub fn solve() {
-    let input = 5719;
+    let input = parse();
     solve1(input);
     solve2(input);
 }
+
+fn parse() -> Vec<i32> {
+    let filename = "input/day1input";
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    contents
+        .lines()
+        .map(|l| l.parse::<i32>().unwrap())
+        .collect()
+}
+
 
 fn solve1(input: i32) {
     println!("{:?},{:?}", 0, 0);
