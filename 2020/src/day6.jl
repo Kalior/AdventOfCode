ca489eed2675dbadd72529802c9a2523d3a65744
Joel Gustafsson <me@joelgustafsson.com>
2020-12-06 06:21:37 +0100

    Day six

---
+++ 2020/src/day6.jl
@@ -0,0 +1,36 @@
+module Day6
+
+include("InputHelper.jl")
+
+function get_input()::Array{String,1}
+    pattern = r"(?<key>\S+):(?<value>\S+)"
+
+    InputHelper.parse(line -> line, "6", "\n\n")
+end
+
+function solve()
+    input = get_input()
+
+    println("Part one $(solve_part_one(input))")
+
+    println("Part two $(solve_part_two(input))")
+end
+
+function solve_part_one(input::Array{String,1})
+    sum(n_questions_answered, input)
+end
+
+function n_questions_answered(group::String)::Int
+    length(Set(c => c for line in split(group, "\n") for c in line))
+end
+
+
+function solve_part_two(input::Array{String,1})
+    sum(all_answered, input)
+end
+
+function all_answered(group::String)::Int
+    length(intersect!([[c for c in line] for line in split(group, "\n") if line != ""]...))
+end
+
+end
