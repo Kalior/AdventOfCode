2b3b8c062666e77dffd1cddf3cc1f9bd7f53f6b1
Joel Gustafsson <me@joelgustafsson.com>
2020-12-10 22:07:54 +0100

    Slight day10 refactor

--- 2020/src/day10.jl
+++ 2020/src/day10.jl
@@ -2,34 +2,32 @@ module Day10
 
 include("InputHelper.jl")
 
-function get_input()::Array{Int,1}
+function get_input()::Array{Int}
     InputHelper.parse(line -> parse(Int, line), "10", "\n")
 end
 
 function solve()
 
     input = get_input()
+    sort!(input)
+    prepend!(input, 0)
 
-    println("Part one $(solve_part_one(input))")
-
-    println("Part two $(solve_part_two(input))")
+    solve_part_one(input), solve_part_two(input)
 end
 
-function solve_part_one(input)
-    sort!(input)
-
+function solve_part_one(input::Array{Int})::Int
     one_jolt_diff = 0
     three_jolt_diff = 0
 
-    current_jolt = 0
-    for i in 1:(length(input))
+    for i in 2:length(input)
+        current_jolt = input[i - 1]
+
         if input[i] <= current_jolt + 3
             if input[i] == current_jolt + 1
                 one_jolt_diff += 1
             elseif input[i] == current_jolt + 3
                 three_jolt_diff += 1
             end
-            current_jolt = input[i]
         end
     end
 
@@ -39,12 +37,7 @@ function solve_part_one(input)
 end
 
 
-function solve_part_two(input)
-    push!(input, 0)
-    sort!(input)
-
-    push!(input, input[end] + 3)
-
+function solve_part_two(input::Array{Int})::Int
     ways_to_get_to_n = Array{Int}(undef, length(input))
     fill!(ways_to_get_to_n, 0)
     ways_to_get_to_n[1] = 1
