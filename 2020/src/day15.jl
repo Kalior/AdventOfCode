f13b980321e3d306f82a033c447bf19fa4da2c6a
Joel Gustafsson <me@joelgustafsson.com>
2020-12-15 08:58:26 +0100

    Refactor 15

--- 2020/src/day15.jl
+++ 2020/src/day15.jl
@@ -7,46 +7,33 @@ function get_input()::Array{Int,1}
 end
 
 function solve()
-
     input = get_input()
 
     solve_part_one(input), solve_part_two(input)
 end
 
 function solve_part_one(input)
-    spoken = Dict(number => (turn) for (turn, number) in enumerate(input))
-    println(spoken)
-
-    current_number = input[end]
-    current_turn = length(input)
-
-    while current_turn < 2020
-        last_turn = get(spoken, current_number, current_turn)
-
-        spoken[current_number] = current_turn
-        current_number = current_turn - last_turn
-
-
-        current_turn += 1
-    end
-    current_number
+    play_game(input, 2020)
 end
 
 
-
 function solve_part_two(input)
-    spoken = Dict(number => (turn) for (turn, number) in enumerate(input))
-    println(spoken)
+    play_game(input, 30000000)
+end
 
-    current_number = input[end]
-    current_turn = length(input)
 
-    while current_turn < 30000000
+function play_game(numbers::Array{Int}, rounds::Int)
+    spoken = Dict(number => turn for (turn, number) in enumerate(numbers))
+
+    current_number = numbers[end]
+    current_turn = length(numbers)
+
+    while current_turn < rounds
         last_turn = get(spoken, current_number, current_turn)
 
         spoken[current_number] = current_turn
-        current_number = current_turn - last_turn
 
+        current_number = current_turn - last_turn
 
         current_turn += 1
     end
