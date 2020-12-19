49834a590b8753d60d739c9ed3fd1834f8aeb3e8
Joel Gustafsson <me@joelgustafsson.com>
2020-12-19 09:50:22 +0100

    Refactor day 19

--- 2020/src/day19.jl
+++ 2020/src/day19.jl
@@ -5,11 +5,22 @@ include("InputHelper.jl")
 function get_input()
     groups =  InputHelper.parse(line -> line, "19", "\n\n")
     split_rules = [split(rule, ": ") for rule in split(groups[1], "\n")]
-    rules = Dict(parse(Int, idx) => rule for (idx, rule) in split_rules)
+    rules = Dict(parse(Int, idx) => parse_rule(rule) for (idx, rule) in split_rules)
 
     messages = split(groups[2], "\n")
 
     (rules, messages)
+end
+
+function parse_rule(rule)
+    if rule == "\"a\""
+        return 'a'
+    elseif rule == "\"b\""
+        return 'b'
+    else
+        subrules = split(rule, " | ")
+        return [[parse(Int, i) for i in split(subrule, " ")] for subrule in subrules]
+    end
 end
 
 function solve()
@@ -21,7 +32,7 @@ end
 function solve_part_one((rules, messages))
     n = 0
     for m in messages
-        matches, last_character_matched = matches_rule(m, 1, (0, rules[0]), rules)
+        matches, last_character_matched = matches_rule(m, 1, rules[0], rules)
         if any(l == length(m) for l in last_character_matched) && matches
             n += 1
         end
@@ -29,69 +40,51 @@ function solve_part_one((rules, messages))
     n
 end
 
-function matches_rule(message, idx, (rule_idx, rule), rules)
+function matches_rule(message, idx,  rule, rules)
     if idx > length(message)
-        return false, [idx]
+        return false, nothing
     end
 
-    if rule == "\"a\""
-        return message[idx] == 'a', [idx]
-    elseif rule == "\"b\""
-        return message[idx] == 'b', [idx]
+    if rule == 'a' || rule == 'b'
+        return message[idx] == rule, [idx]
     else
-        subrules = split(rule, " | ")
-        subrules = [[parse(Int, i) for i in split(subrule, " ")] for subrule in subrules]
+        possible_subrules = (match_subrule(message, idx, subrule, rules) for subrule in rule)
 
-        subrules = ugly_deal_with_repeats((rule_idx, rule), subrules)
+        all_matching_indicies = vcat([indicies for (match, indicies) in possible_subrules if match]...)
 
-        matches = false
-        all_sub_idxs = []
-
-        for subrule in subrules
-            matches_subrule = true
-            sub_idxes = [idx - 1]
-            for sub in subrule
-                match = true
-                new_sub_idxes = []
-                for sub_idx in sub_idxes
-                    other_match, other_new_sub_idxes = matches_rule(message, sub_idx + 1, (sub, rules[sub]), rules)
-                    if other_match
-                        match = true
-                        append!(new_sub_idxes, other_new_sub_idxes)
-                    end
-                end
-
-                if !match
-                    matches_subrule = false
-                    break
-                else
-                    sub_idxes = new_sub_idxes
-                end
-            end
-
-            if matches_subrule
-                matches = true
-                append!(all_sub_idxs, sub_idxes)
-            end
-        end
-
-        return matches, all_sub_idxs
+        return length(all_matching_indicies) != 0, all_matching_indicies
     end
 
-    false, [0]
+    false, nothing
 end
 
-function ugly_deal_with_repeats((rule_idx, rule), subrules)
-    if length(subrules) == 1
-        return subrules
-    elseif !in(rule_idx, subrules[2])
-        return subrules
+function match_subrule(message, idx, subrule, rules)
+    sub_idxes = [idx - 1]
+
+    for sub in subrule
+        possible_matches = (matches_rule(message, sub_idx + 1, rules[sub], rules) for sub_idx in sub_idxes)
+
+        sub_idxes = vcat([array for (match, array) in possible_matches if match]...)
+
+        if length(sub_idxes) == 0
+            return false, nothing
+        end
+    end
+
+    true, sub_idxes
+end
+
+function ugly_deal_with_repeats(rule_idx, rule)
+    if length(rule) == 1
+        return rule
+    elseif !in(rule_idx, rule[2])
+        return rule
     else
-        first_subrule = subrules[1]
+        first_subrule = rule[1]
         new_subrules = [first_subrule]
-        for i in 1:100
+        for i in 1:10
             new_subrule = []
-            for r in subrules[2]
+            for r in rule[2]
                 if r != rule_idx
                     push!(new_subrule, r)
                 else
@@ -108,12 +101,12 @@ function ugly_deal_with_repeats((rule_idx, rule), subrules)
 end
 
 function solve_part_two((rules, messages))
-    rules[8] = "42 | 42 8"
-    rules[11] = "42 31 | 42 11 31"
+    rules[8] = ugly_deal_with_repeats(8, parse_rule("42 | 42 8"))
+    rules[11] = ugly_deal_with_repeats(11, parse_rule("42 31 | 42 11 31"))
     n = 0
     for m in messages
-        matches, last_character_matched = matches_rule(m, 1, (0, rules[0]), rules)
-        println("$m $matches")
+        matches, last_character_matched = matches_rule(m, 1, rules[0], rules)
+        # println("$m $matches")
         if any(l == length(m) for l in last_character_matched) && matches
             n += 1
         end
