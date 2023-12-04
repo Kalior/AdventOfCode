a18d37b1873503e76d56868c4910843c682668c5
Joel Gustafsson <joel.gustafsson@tenfifty.io>
2023-12-04 08:10:43 +0100

    Day 4 part 2

--- aoc-2023/day4.py
+++ aoc-2023/day4.py
@@ -1,10 +1,5 @@
 from pathlib import Path
-from itertools import count
-from dataclasses import dataclass, field
-from typing import Iterable
-from math import prod
-
-from more_itertools import split_at
+import numpy as np
 
 
 def parse_input() -> list[tuple[set[int], set[int]]]:
@@ -12,13 +7,11 @@ def parse_input() -> list[tuple[set[int], set[int]]]:
     with p.open("r") as f:
         input = f.read()
 
-
     def parse_group(group: str) -> list[int]:
         return [int(v) for v in group.split()]
 
     def parse_groups(groups: str) -> tuple[set[int], set[int]]:
         return tuple([set(parse_group(g)) for g in groups.split(" | ")])
-
 
     return [parse_groups(line.split(": ")[1]) for line in input.splitlines()]
 
@@ -27,15 +20,19 @@ def solve(scratchcards: list[tuple[set[int], set[int]]]):
     points = 0
     for winning, mine in scratchcards:
         same = winning.intersection(mine)
-        print(2 ** (len(same) - 1))
         if len(same) > 0:
             points += 2 ** (len(same) - 1)
 
     return points
 
 
-def solve_2(schema: list[tuple[set[int], set[int]]]):
-    pass
+def solve_2(scratchcards: list[tuple[set[int], set[int]]]):
+    weights = np.ones(len(scratchcards), dtype=int)
+    for i, (winning, mine) in enumerate(scratchcards):
+        n_same = len(winning.intersection(mine))
+        weights[i + 1 : i + n_same + 1] += weights[i]
+
+    return weights.sum()
 
 
 if __name__ == "__main__":
