818c0d53ff7327a70c45d279c0e2c4a6929d51c7
Joel Gustafsson <joel.gustafsson@tenfifty.io>
2024-12-01 20:30:52 +0100

    Extract function

--- aoc-2024/src/day1.zig
+++ aoc-2024/src/day1.zig
@@ -11,18 +11,14 @@ fn readFile(allocator: std.mem.Allocator, filename: []const u8) ![]u8 {
     return try file.readToEndAlloc(allocator, stat.size);
 }
 
-pub fn solve() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-
-    const input = try readFile(allocator, "inputs/day1");
+fn parseToLists(allocator: std.mem.Allocator, input: []u8) !struct { left: std.ArrayList(i32), right: std.ArrayList(i32) } {
     var lines = std.mem.split(u8, input, "\n");
 
     var left_list = std.ArrayList(i32).init(allocator);
-    defer left_list.deinit();
+    errdefer left_list.deinit();
 
     var right_list = std.ArrayList(i32).init(allocator);
-    defer right_list.deinit();
+    errdefer right_list.deinit();
 
     while (lines.next()) |line| {
         if (line.len == 0) {
@@ -38,11 +34,23 @@ pub fn solve() !void {
         try right_list.append(right_v);
     }
 
-    std.mem.sort(i32, left_list.items, {}, comptime std.sort.asc(i32));
-    std.mem.sort(i32, right_list.items, {}, comptime std.sort.asc(i32));
+    return .{ .left = left_list, .right = right_list };
+}
+
+pub fn solve() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const input = try readFile(allocator, "inputs/day1");
+    var lists = try parseToLists(allocator, input);
+    defer lists.left.deinit();
+    defer lists.right.deinit();
+
+    std.mem.sort(i32, lists.left.items, {}, comptime std.sort.asc(i32));
+    std.mem.sort(i32, lists.right.items, {}, comptime std.sort.asc(i32));
 
     var total_distance: i32 = 0;
-    for (left_list.items, right_list.items) |left, right| {
+    for (lists.left.items, lists.right.items) |left, right| {
         total_distance += @intCast(@abs(left - right));
     }
 
@@ -51,16 +59,20 @@ pub fn solve() !void {
     var frequencies = std.AutoHashMap(i32, i32).init(allocator);
     defer frequencies.deinit();
 
-    for (right_list.items) |right| {
+    for (lists.right.items) |right| {
         const old = frequencies.get(right) orelse 0;
 
         try frequencies.put(right, old + 1);
     }
 
     var similarity_score: i32 = 0;
-    for (left_list.items) |left| {
+    for (lists.left.items) |left| {
         const freq = frequencies.get(left) orelse 0;
         similarity_score += left * freq;
     }
     std.debug.print("Similarity score {}.\n", .{similarity_score});
+}
+
+pub fn main() !void {
+    try solve();
 }
